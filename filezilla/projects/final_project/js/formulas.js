cb5bbd0bd7da18faa1d740c10038488a31f31e8c
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-12-07 11:57:38 +0100

    2023-12-07

--- filezilla/projects/final_project/js/formulas.js
+++ filezilla/projects/final_project/js/formulas.js
@@ -1,25 +1,29 @@
-let variables_in_formulas = [
-  ["s", "v", "t"],
-  ["v", "v0", "a", "t"],
-  ["s", "v0", "t", "a"],
-  ["v", "v0", "a", "s"],
-  ["F", "m", "a"],
-];
-
-let 
+let variables_in_formulas = {
+  "s = v*t": ["s", "v", "t"],
+  "v = v0 + a*t": ["v", "v0", "a", "t"],
+  "s = v0*t + (a*t**2)/2": ["s", "v0", "t", "a"],
+  "v**2 - v0**2 = 2*a*s": ["v", "v0", "a", "s"],
+  "F = m*a": ["F", "m", "a"],
+};
 
 function main() {
-  const distance = parseInt(document.querySelector("#distance").value);
-  const currentVelocity = parseInt(
-    document.querySelector("#currentVelocity").value
-  );
-  const acceleration = parseInt(document.querySelector("#acceleration").value);
-  const time = parseInt(document.querySelector("#time").value);
-  const initialVelocity = parseInt(
-    document.querySelector("#initialVelocity").value
-  );
-  const mass = parseInt(document.querySelector("#mass").value);
-  const force = parseInt(document.querySelector("#force").value);
+  const distance = document.querySelector("#distance").value.length
+    ? "s"
+    : undefined;
+  const currentVelocity = document.querySelector("#currentVelocity").value
+    .length
+    ? "v"
+    : undefined;
+  const acceleration = document.querySelector("#acceleration").value.length
+    ? "a"
+    : undefined;
+  const time = document.querySelector("#time").value.length ? "t" : undefined;
+  const initialVelocity = document.querySelector("#initialVelocity").value
+    .length
+    ? "v0"
+    : undefined;
+  const mass = document.querySelector("#mass").value.length ? "m" : undefined;
+  const force = document.querySelector("#force").value.length ? "F" : undefined;
   const search = document.querySelector("input[name=search]:checked").value;
   const usedVariables = [
     distance,
@@ -29,38 +33,44 @@ function main() {
     initialVelocity,
     mass,
     force,
-  ].filter((variable) => !isNaN(variable));
-  let a = false;
-  let possible_formulas = []
-  for (let i of variables_in_formulas) {
-    if (i.indexOf(search) >= 0) {
-      possible_formulas.push(i)
-    }
-  }
+  ].filter(Boolean);
+  let formula = findFormula(usedVariables, search);
+  let key = Object.keys(variables_in_formulas).find(
+    (k) => variables_in_formulas[k] === formula
+  );
+  console.log(search)
+  let finalFormula = rewriteFormula(key, search);
+  console.log(finalFormula);
+}
+
+function findFormula(usedVariables, search) {
+  let possible_formulas = Object.values(variables_in_formulas).filter(
+    (formula) => formula.indexOf(search) != -1
+  );
   console.log(possible_formulas)
-  outer: for (let i of variables_in_formulas) {
-    console.log(i)
+  outer: for (let i of possible_formulas) {
     for (let j of i) {
-      console.log(j)
-      console.log(usedVariables.indexOf(j));
-      if (usedVariables.indexOf(j) == -1) {
+      if (usedVariables.indexOf(j) == -1 && j != search) {
         continue outer;
       }
     }
-    if (i.indexOf(search) >= 0) {
-      a = true;
+    return i;
+  }
+  return RecursiveSearch(usedVariables, possible_formulas, search);
+}
+
+function RecursiveSearch(usedVariables, possible_formulas, search) {
+  console.log(usedVariables)
+  for (let i of possible_formulas) {
+    for (let j of i.filter(
+      (x) => usedVariables.indexOf(x) == -1 && x != search
+    )) {
+      return findFormula(usedVariables, j);
     }
   }
-  if (a) {
-    console.log("It can be solved");
-  }
 }
 
-function findVariable() {
-
-}
-
-function rewriteVariable(formula, searchedVariable) {
+function rewriteFormula(formula, searchedVariable) {
   if (formula == "s = v*t") {
     if (searchedVariable == "s") {
       return ["s = v*t"];
@@ -84,6 +94,10 @@ function rewriteVariable(formula, searchedVariable) {
       return ["s = v0*t + (a*t**2)/2"];
     } else if (searchedVariable == "v0") {
       return [""];
+    }
+  } else if (formula == "F = m*a") {
+    if (searchedVariable == "F") {
+      return ["F = m*a"];
     }
   }
 }
